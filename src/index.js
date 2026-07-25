7a68bbdb80c9fa6ca8485f955478c85a7fb12009
Tiya kumari <Tiyatiwari@users.noreply.github.com>
2026-07-25 14:37:17 +0530

    create 2 home comonenets awards and hero

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Homepage from './landing_page/home/HomePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-    <h1>hello</h1>  
-    </div>
+   
+       <Homepage />
+   
   </React.StrictMode>
 );
 
