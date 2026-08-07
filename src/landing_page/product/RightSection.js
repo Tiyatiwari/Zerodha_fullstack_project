2321b678a85df6a2787fe1b22f57b8e26eb6fd5e
Tiya kumari <Tiyatiwari@users.noreply.github.com>
2026-08-07 16:50:19 +0530

    product section done

--- src/landing_page/product/RightSection.js
+++ src/landing_page/product/RightSection.js
@@ -1,7 +1,42 @@
 import React from "react";
 
-function RightSection() {
-    return ( <h1>rightsection</h1>  );
+function RightSection({
+  imageURL,
+  productName,
+  productDesription,
+  learnMore,
+}) {
+  return (
+    <div className="container mt-5">
+      <div className="row align-items-center">
+        {/* Text Section */}
+        <div className="col-12 col-md-6 col-lg-5 p-3 p-md-5 order-2 order-md-1">
+          <h1>{productName}</h1>
+
+          <p className="mt-4">{productDesription}</p>
+
+          <div className="mt-4">
+            <a href={learnMore} className="product-link">
+              Learn More{" "}
+              <i
+                className="fa fa-long-arrow-right ms-2"
+                aria-hidden="true"
+              ></i>
+            </a>
+          </div>
+        </div>
+
+        {/* Image Section */}
+        <div className="col-12 col-md-6 col-lg-6 offset-lg-1 text-center mb-4 mb-md-0 order-1 order-md-2">
+          <img
+            src={imageURL}
+            className="img-fluid"
+            alt={productName}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default RightSection;
--- src/landing_page/product/Universe.js
+++ src/landing_page/product/Universe.js
@@ -1,6 +1,105 @@
 import React from "react";
 
 function Universe() {
-    return ( <h1>universe</h1>);
+  return (
+    <div className="container mt-5">
+      <div className="row text-center">
+        <h1 style={{ color: "#424242" }}>The Zerodha Universe</h1>
+
+        <p className="text-muted mt-3">
+          Extend your trading and investment experience even further with our
+          partner platforms
+        </p>
+
+        <div className="col-12 col-md-6 col-lg-4 p-4 mt-4">
+          <img
+            src="media/images/smallcaseLogo.png"
+            alt="Smallcase"
+            className="img-fluid"
+            style={{ maxWidth: "170px" }}
+          />
+          <p className="text-muted mt-3" style={{ fontSize: "0.9rem" }}>
+            Thematic investment platform
+          </p>
+        </div>
+
+        <div className="col-12 col-md-6 col-lg-4 p-4 mt-4">
+          <img
+            src="media/images/streakLogo.png"
+            alt="Streak"
+            className="img-fluid"
+            style={{ maxWidth: "170px" }}
+          />
+          <p className="text-muted mt-3" style={{ fontSize: "0.9rem" }}>
+            Algo & strategy platform
+          </p>
+        </div>
+
+        <div className="col-12 col-md-6 col-lg-4 p-4 mt-4">
+          <img
+            src="media/images/sensibullLogo.svg"
+            alt="Sensibull"
+            className="img-fluid"
+            style={{ maxWidth: "170px" }}
+          />
+          <p className="text-muted mt-3" style={{ fontSize: "0.9rem" }}>
+            Options trading platform
+          </p>
+        </div>
+
+        <div className="col-12 col-md-6 col-lg-4 p-4 mt-4">
+          <img
+            src="media/images/zerodhafundhouse.png"
+            alt="Zerodha Fund House"
+            className="img-fluid"
+            style={{ maxWidth: "170px" }}
+          />
+          <p className="text-muted mt-3" style={{ fontSize: "0.9rem" }}>
+            Asset management
+          </p>
+        </div>
+
+        <div className="col-12 col-md-6 col-lg-4 p-4 mt-4">
+          <img
+            src="media/images/goldenpiLogo.png"
+            alt="GoldenPi"
+            className="img-fluid"
+            style={{ maxWidth: "170px" }}
+          />
+          <p className="text-muted mt-3" style={{ fontSize: "0.9rem" }}>
+            Bonds trading platform
+          </p>
+        </div>
+
+        <div className="col-12 col-md-6 col-lg-4 p-4 mt-4">
+          <img
+            src="media/images/dittoLogo.png"
+            alt="Ditto"
+            className="img-fluid"
+            style={{ maxWidth: "170px" }}
+          />
+          <p className="text-muted mt-3" style={{ fontSize: "0.9rem" }}>
+            Insurance
+          </p>
+        </div>
+
+        <div className="mt-5 mb-5">
+          <button
+            className="zerodha-btn p-2 fs-5 mb-5"
+            style={{
+              padding: "12px 40px",
+              margin: "0 auto",
+              whiteSpace: "nowrap",
+              display: "inline-block",
+              width: "fit-content",
+            }}
+          >
+            Signup now
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default Universe;
